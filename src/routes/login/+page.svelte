1fca28336bd7000438c19933caf4cd2240c60e62
joyhabegger <habeggerjoy@gmail.com>
2023-06-12 22:50:29 -0400

    add form reset

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,27 +3,36 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-	const { form, errors, enhance } = superForm(data.form);
+	const { form, errors, enhance, reset } = superForm(data.form);
+	function resetForm() {
+		reset({ data: { email: '', password: '' } });
+	}
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center px-10">
-	<form method="post" use:enhance>
+	<form method="post" use:enhance novalidate>
 		<h2 class="h2 mb-6">Log in to our new app!</h2>
 		<label class="label mt-8" for="email">
 			<span class="font-bold">Email</span>
-			<input class="input" type="email" name="email" bind:value={$form.email} />
+			<input class="input" type="email" name="email" id="email" bind:value={$form.email} />
 			{#if $errors.email}
 				<span class="block text-red-600 dark:text-red-500">{$errors.email}</span>
 			{/if}
 		</label>
 		<label class="label mt-8" for="password">
 			<span class="font-bold">Password</span>
-			<input class="input" type="password" name="password" bind:value={$form.password} />
+			<input
+				class="input"
+				type="password"
+				name="password"
+				id="password"
+				bind:value={$form.password}
+			/>
 			{#if $errors.password}
 				<span class="block text-red-600 dark:text-red-500">{$errors.password}</span>
 			{/if}
 		</label>
 		<button type="submit" class="btn variant-filled mt-8">Log in</button>
-		<button type="reset" class="btn variant-filled-error mt-8">Reset</button>
+		<button type="reset" on:click={resetForm} class="btn variant-filled-error mt-8">Reset</button>
 	</form>
 </div>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -3,22 +3,31 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-	const { form, errors, enhance } = superForm(data.form);
+	const { form, errors, enhance, reset } = superForm(data.form);
+	function resetForm() {
+		reset({ data: { email: '', password: '', passwordConfirm: '' } });
+	}
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center px-10">
-	<form method="post" use:enhance>
+	<form method="post" use:enhance novalidate>
 		<h2 class="h2 mb-6">Sign up our new app!</h2>
 		<label class="label mt-8" for="email">
 			<span class="font-bold">Email</span>
-			<input class="input" type="email" name="email" bind:value={$form.email} />
+			<input class="input" type="email" name="email" id="email" bind:value={$form.email} />
 			{#if $errors.email}
 				<span class="block text-red-600 dark:text-red-500">{$errors.email}</span>
 			{/if}
 		</label>
 		<label class="label mt-8" for="password">
 			<span class="font-bold">Password</span>
-			<input class="input" type="password" name="password" bind:value={$form.password} />
+			<input
+				class="input"
+				type="password"
+				name="password"
+				id="password"
+				bind:value={$form.password}
+			/>
 			{#if $errors.password}
 				<span class="block text-red-600 dark:text-red-500">{$errors.password}</span>
 			{/if}
@@ -29,6 +38,7 @@
 				class="input"
 				type="password"
 				name="passwordConfirm"
+				id="passwordConfirm"
 				bind:value={$form.passwordConfirm}
 			/>
 			{#if $errors.passwordConfirm}
@@ -36,6 +46,6 @@
 			{/if}
 		</label>
 		<button type="submit" class="btn variant-filled mt-8">Sign up</button>
-		<button type="reset" class="btn variant-filled-error mt-8">Reset</button>
+		<button type="reset" on:click={resetForm} class="btn variant-filled-error mt-8">Reset</button>
 	</form>
 </div>
